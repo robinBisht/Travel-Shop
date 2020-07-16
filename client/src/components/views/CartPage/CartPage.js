9d79eb1dc32252b9f488a380912087d052a644ff
robinBisht <robinbisht7@gmail.com>
2020-07-17 00:13:06 +0530

    Adding Paypal Functionality

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,47 +1,50 @@
-import React,{useEffect,useState} from 'react'
-import {useDispatch} from 'react-redux'
-import {getCartItems,removeCartItem} from '../../../_actions/user_actions'
-import UserCardBlock from './Sections/UserCardBlock'
-import {Result,Empty} from 'antd'; 
-
-
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import {
+    getCartItems,
+    removeCartItem,
+    onSuccessBuy
+} from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
+import { Result, Empty } from 'antd';
+import Axios from 'axios';
+import Paypal from '../../utils/Paypal';
 function CartPage(props) {
-    const dispatch = useDispatch()
-    const [Total,setTotal] = useState(0);
-    const [ShowTotal,setShowTotal] = useState(false);
-    const [ShowSuccess,setShowSuccess] = useState(false)
+    const dispatch = useDispatch();
+    const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
+    const [ShowSuccess, setShowSuccess] = useState(false)
+
+    useEffect(() => {
+
+        let cartItems = [];
+        if (props.user.userData && props.user.userData.cart) {
+            if (props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach(item => {
+                    cartItems.push(item.id)
+                });
+                dispatch(getCartItems(cartItems, props.user.userData.cart))
+                    .then((response) => {
+                        if (response.payload.length > 0) {
+                            calculateTotal(response.payload)
+                        }
+                    })
+            }
+        }
+
+    }, [props.user.userData])
 
     const calculateTotal = (cartDetail) => {
         let total = 0;
 
         cartDetail.map(item => {
-            total += parseInt(item.price,10)*item.quantity
-        })
+            total += parseInt(item.price, 10) * item.quantity
+        });
 
         setTotal(total)
         setShowTotal(true)
-
     }
 
-    useEffect(() => {
-        let cartItems = [];
-        if(props.user.userData && props.user.userData.cart)
-        {
-            if(props.user.userData.cart.length > 0)
-            {
-                props.user.userData.cart.forEach(item => {
-                    cartItems.push(item.id)
-                })
-                dispatch(getCartItems(cartItems, props.user.userData.cart))
-            }
-        }
-    },[props.user.userData])
-
-    useEffect(() => {
-        if(props.user.cartDetail && props.user.cartDetail.length > 0) {
-            calculateTotal(props.user.cartDetail)
-        }
-    })
 
     const removeFromCart = (productId) => {
 
@@ -55,6 +58,28 @@ function CartPage(props) {
             })
     }
 
+    const transactionSuccess = (data) => {
+        dispatch(onSuccessBuy({
+            cartDetail: props.user.cartDetail,
+            paymentData: data
+        }))
+            .then(response => {
+                if (response.payload.success) {
+                    setShowSuccess(true)
+                    setShowTotal(false)
+                }
+            })
+    }
+
+    const transactionError = () => {
+        console.log('Paypal error')
+    }
+
+    const transactionCanceled = () => {
+        console.log('Transaction canceled')
+    }
+
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
@@ -65,19 +90,17 @@ function CartPage(props) {
                     removeItem={removeFromCart}
                 />
 
-            {ShowTotal ? 
-                <div style={{ marginTop: '3rem' }}>
-                <h2>Total amount: ${Total} </h2>
-            </div>
 
-            :
-            ShowSuccess ?
-                <Result
-                status="success"
-                title="Successfully Purchased Items"
-            /> 
-
-            :
+                {ShowTotal ?
+                    <div style={{ marginTop: '3rem' }}>
+                        <h2>Total amount: ${Total} </h2>
+                    </div>
+                    :
+                    ShowSuccess ?
+                        <Result
+                            status="success"
+                            title="Successfully Purchased Items"
+                        /> :
                         <div style={{
                             width: '100%', display: 'flex', flexDirection: 'column',
                             justifyContent: 'center'
@@ -87,9 +110,26 @@ function CartPage(props) {
                             <p>No Items In the Cart</p>
 
                         </div>
-            
-            }  
+                }
             </div>
+
+
+
+            {/* Paypal Button */}
+
+            {ShowTotal &&
+
+                <Paypal
+                    toPay={Total}
+                    onSuccess={transactionSuccess}
+                    transactionError={transactionError}
+                    transactionCanceled={transactionCanceled}
+                />
+
+            }
+
+
+
         </div>
     )
 }
